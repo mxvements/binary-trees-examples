cab96af4160d062179ba73c3a311dc66c02760b7
mxvements <luciama2@student.42madrid.com>
2024-02-12 17:09:42 +0100

    finished main functions for binary tree

--- binary_trees.c
+++ binary_trees.c
@@ -6,12 +6,14 @@
 	void	ft_lstadd_back(t_list **lst, t_list *new);
 	int		ft_lstsize(t_list *lst);
 	t_list	*ft_lstlast(t_list *lst);
-	void	ft_lstdelone(t_list *lst, void (*del)(void *));
 	void	ft_lstclear(t_list **lst, void (*del)(void *));
+
+	void	ft_lstdelone(t_list *lst, void (*del)(void *));
 	void	ft_lstiter(t_list *lst, void (*f)(void *));
 	t_list	*ft_lstmap(t_list *lst, void *(*f)(void *), void (*del)(void *));
 */
 
+/* TREE NEW NODE */
 t_tree	*ft_treenew(void *content)
 {
 	t_tree	*treenode;
@@ -24,6 +26,7 @@ t_tree	*ft_treenew(void *content)
 	treenode->right = NULL;
 	return (treenode);
 }
+/* TREE ADD */
 
 void	ft_treeadd_left(t_tree **tree, t_tree *new)
 {
@@ -45,6 +48,80 @@ void	ft_treeadd_right(t_tree **root, t_tree *new)
 	node->right = new;
 }
 
+/* TREE LEAF (LAST) && TREE DEPTH */
+int		ft_treedepth_recursive(t_tree **root, t_tree *target, int depth)
+{
+	int	depth_left;
+	int	depth_right;
+
+	if (*root == target)
+		return (depth);
+	if (!(*root))
+		return (-1);
+	depth++;
+	depth_left = ft_treedepth_recursive(&((*root)->left), target, depth);
+	depth_right = ft_treedepth_recursive(&((*root)->right), target, depth);
+	if (depth_left > 0)
+		return (depth_left);
+	return (depth_right);
+}
+int		ft_treedepth(t_tree **root, t_tree *target)
+{
+	return (ft_treedepth_recursive(root, target, 0));
+}
+
+t_tree	*ft_treelast(t_tree **root)
+{
+	t_tree	*node;
+	t_tree 	*last_left;
+	t_tree	*last_right;
+	int		last_left_depth;
+	int		last_right_depth;
+
+	node = *root;
+	last_left_depth = 0;
+	last_right_depth = 0;
+	if (!node->left && !node->right)
+		return (node);
+	if (node->left)
+	{
+		last_left = ft_treelast(&(node->left));
+		last_left_depth = ft_treedepth(root, last_left);
+	}
+	if (node->right)
+	{
+		last_right = ft_treelast(&(node->right));
+		last_right_depth = ft_treedepth(root, last_right);
+	}
+	if (last_left_depth >= last_right_depth)
+		return (last_left);
+	return (last_right);
+}
+
+/* TREE SIZE RECURSIVE */
+
+int	ft_treesize_recursive(t_tree **root, int *count)
+{
+	t_tree	*node;
+
+	node = *root;
+	if (!node)
+		return (*count);
+	(*count)++;
+	ft_treesize_recursive(&(node->left), count);
+	ft_treesize_recursive(&(node->right), count);
+	return (*count);
+}
+int	ft_treesize(t_tree **root)
+{
+	int	count;
+
+	count = 0;
+	return (ft_treesize_recursive(root, &count));
+}
+
+/* PRINT TREE RECURSIVE */
+
 static void	printtabs(int numtabs)
 {
 	for (int i = 0; i < numtabs; i++)
@@ -52,7 +129,6 @@ static void	printtabs(int numtabs)
 		printf("\t");
 	}
 }
-
 void	ft_treeprint_recursive(t_tree **root, int level)
 {
 	t_tree		*node;
@@ -91,10 +167,31 @@ void	ft_treeprint_recursive(t_tree **root, int level)
 	printf("done\n");
 	return ;
 }
-
 void	ft_treeprint(t_tree **root)
 {
 	return ft_treeprint_recursive(root, 0);
+}
+
+/* TREE CLEAR */
+//void	ft_lstclear(t_list **lst, void (*del)(void *));
+
+void	del(void *content)
+{
+	free(content);
+}
+
+void	ft_treeclear(t_tree **root, void (*del)(void  *))
+{
+	if (!(*root))
+		return;
+	if ((*root)->left)
+		ft_treeclear(&((*root)->left), del);
+	if ((*root)->right)
+		ft_treeclear(&((*root)->right), del);
+	del((*root)->content);
+	(*root)->content = NULL;
+	free(*root);
+	(*root) = NULL;
 }
 
 int	main(void)
@@ -112,25 +209,25 @@ int	main(void)
 	*/
 
 	//content
-	t_content		c1;
-	c1.nbr = 10;
-	t_content		c2;
-	c2.nbr = 11;
-	t_content		c3;
-	c3.nbr = 12;
-	t_content		c4;
-	c4.nbr = 13;
-	t_content		c5;
-	c5.nbr = 14;
-	t_content		c6;
-	c6.nbr = 15;
+	t_content		*c1 = malloc(sizeof(t_content));
+	c1->nbr = 10;
+	t_content		*c2 = malloc(sizeof(t_content));
+	c2->nbr = 11;
+	t_content		*c3 = malloc(sizeof(t_content));
+	c3->nbr = 12;
+	t_content		*c4 = malloc(sizeof(t_content));
+	c4->nbr = 13;
+	t_content		*c5 = malloc(sizeof(t_content));
+	c5->nbr = 14;
+	t_content		*c6 = malloc(sizeof(t_content));
+	c6->nbr = 15;
 	//nodes
-	t_tree	*n1 = ft_treenew((void *)&c1);
-	t_tree	*n2 = ft_treenew((void *)&c2);
-	t_tree	*n3 = ft_treenew((void *)&c3);
-	t_tree	*n4 = ft_treenew((void *)&c4);
-	t_tree	*n5 = ft_treenew((void *)&c5);
-	t_tree	*n6 = ft_treenew((void *)&c6);
+	t_tree	*n1 = ft_treenew((void *)c1);
+	t_tree	*n2 = ft_treenew((void *)c2);
+	t_tree	*n3 = ft_treenew((void *)c3);
+	t_tree	*n4 = ft_treenew((void *)c4);
+	t_tree	*n5 = ft_treenew((void *)c5);
+	t_tree	*n6 = ft_treenew((void *)c6);
 
 	//define root
 	t_tree **root = &n1; 
@@ -145,12 +242,26 @@ int	main(void)
 	//print tree
 	ft_treeprint(root);
 
+	//print size of tree
+	printf("size of tree: %d \n", ft_treesize(root));
+
+	//print last
+	t_content	*last_content = ft_treelast(root)->content;
+	printf("last node %d\n", last_content->nbr);
+
+	//printf depth
+	printf("depth last node: %d\n", ft_treedepth(root, ft_treelast(root)));
+
+	/*
 	//free
 	free(n1);
 	free(n2);
 	free(n3);
 	free(n4);
 	free(n5);
-	free(n6);
+	free(n6);*/
+
+	//tree clear
+	ft_treeclear(root, del);
 	return (0);
 }
